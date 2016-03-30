c004b95710aff8476d051e22c0144c8ec7e8acc6
Nick Perez <nperez0111@gmail.com>
2016-03-29 21:40:14 -0400

    Just to prettify

--- Motor Controller/src/org/usfirst/frc/team5329/robot/Robot.java
+++ Motor Controller/src/org/usfirst/frc/team5329/robot/Robot.java
@@ -21,8 +21,8 @@ import edu.wpi.first.wpilibj.buttons.Button;
  * values are only transmitted from the Driver Station once every 20ms.
  */
 public class Robot extends SampleRobot {
-	
-    private SpeedController leftSide;	// the motor to directly control with a joystick
+    
+    private SpeedController leftSide;   // the motor to directly control with a joystick
     private SpeedController rightSide;
     private SpeedController rightLauncher;
     private SpeedController leftLauncher;
@@ -35,16 +35,16 @@ public class Robot extends SampleRobot {
     
     
 
-	private final double k_updatePeriod = 0.005; // update every 0.005 seconds/5 milliseconds (200Hz)
+    private final double k_updatePeriod = 0.005; // update every 0.005 seconds/5 milliseconds (200Hz)
 
-	//**************************************
-	//Setting new instances of Robot Objects
-	//**************************************
+    //**************************************
+    //Setting new instances of Robot Objects
+    //**************************************
     public Robot() {
-    	
-    	//driveTrain = new RobotDrive(leftSide, rightSide); //speed controllers drive
-    	
-        leftSide = new Victor(0);	
+        
+        //driveTrain = new RobotDrive(leftSide, rightSide); //speed controllers drive
+        
+        leftSide = new Victor(0);   
         rightSide = new Victor(1);
         
         rightLauncher = new VictorSP(4);
@@ -68,67 +68,67 @@ public class Robot extends SampleRobot {
     //ACCORDING TO THIS:
     
      /*public void autonomous() {
-    	 while(isAutonomous() && isEnabled())
-    	 {
-    	 
-    	//****************
+         while(isAutonomous() && isEnabled())
+         {
+         
+        //****************
         //Driving up to the obstacle (2pts)
-    	//****************
-    		 
-    	for(int i = 0; i < 4; i++)
-    		{
-    			driveTrain.drive(0.2, 0.0);
-    			Timer.delay(2.0);
-    		}
-    	
-    	driveTrain.drive(0.0, 0.0);
-    		
+        //****************
+             
+        for(int i = 0; i < 4; i++)
+            {
+                driveTrain.drive(0.2, 0.0);
+                Timer.delay(2.0);
+            }
+        
+        driveTrain.drive(0.0, 0.0);
+            
     //******************************
     //Switch cases for the obstacles
     //******************************
-    		 
-    	switch(obstacle)
-    	{
-    	//Moat 
-    		case 1: 
-    	//Ramparts	
-    		case 2:
-    	//Rock Wall	
-    		case 3:
-    	//Rough Terrain	
-    		case 4:
-    	//Cheval de Frise	
-    		case 5:
-    	//Sally Port	
-    		case 6:
-    	//Draw Bridge	
-    		case 7:
-    	 	
-    		default:
-	 
-    	}	 
-    		 
-    		 
-    		 
+             
+        switch(obstacle)
+        {
+        //Moat 
+            case 1: 
+        //Ramparts  
+            case 2:
+        //Rock Wall 
+            case 3:
+        //Rough Terrain 
+            case 4:
+        //Cheval de Frise   
+            case 5:
+        //Sally Port    
+            case 6:
+        //Draw Bridge   
+            case 7:
+            
+            default:
+     
+        }    
+             
+             
+             
     //******************************************** 
     //Switch cases for positioning after obstacles 
-	//********************************************.
-	  	switch(position)
-	  	{
-	  	//Position 1
-	  		case 1:
-	  	//Position 2	
-	  		case 2:
-	  	//Position 3	
-	  		case 3:
-	  	//Position 4	
-	  		case 4:
-	  	//Stop moving after traversing obstacle	
-	  		default:
-	  	
-	  	}
-	  	
-    	 }
+    //********************************************.
+        switch(position)
+        {
+        //Position 1
+            case 1:
+        //Position 2    
+            case 2:
+        //Position 3    
+            case 3:
+        //Position 4    
+            case 4:
+        //Stop moving after traversing obstacle 
+            default:
+        
+        }
+        
+         }
      }
      
     
@@ -139,115 +139,115 @@ public class Robot extends SampleRobot {
     //*****************
  
     public void operatorControl() {
-    	//getWatchdog().setEnabled(true);
+        //getWatchdog().setEnabled(true);
         while (isOperatorControl() && isEnabled()) {
-        	
-        	double launcherSpeed = 1;
-        	
-        	//******************
-        	//Sticks to move Carl
-        	//******************
-        	
-        	leftSide.set((stick.getY()) * (.7));
-        	
-        	leftSide.set((stick.getY()) * (-.7));
-        	
-        	rightSide.set(stick.getRawAxis(3) * (-.7));
-        	
-        	rightSide.set(stick.getRawAxis(3) * (.7));
-        	
-        	//*****************
-        	//if(stick.getRawButton(4) == true)//My launcher code
-        	//{
-        	//pickUp.set(-0.4);
-        	//}
-        	
-        	if(stick.getRawButton(1)){//
-        		launcherSpeed = 0.8;
-        		System.out.print("button "+1+" Fired!");	
-        	}
-        	else if(stick.getRawButton(2)){//
-        		launcherSpeed = 0.85;
-        		System.out.print("button "+2+" Fired!");
-        		}
-        	else if(stick.getRawButton(3)){//
-        		launcherSpeed = 0.95;
-        	System.out.print("button "+3+" Fired!");
+            
+            double launcherSpeed = 1;
+            
+            //******************
+            //Sticks to move Carl
+            //******************
+            
+            leftSide.set((stick.getY()) * (.7));
+            
+            leftSide.set((stick.getY()) * (-.7));
+            
+            rightSide.set(stick.getRawAxis(3) * (-.7));
+            
+            rightSide.set(stick.getRawAxis(3) * (.7));
+            
+            //*****************
+            //if(stick.getRawButton(4) == true)//My launcher code
+            //{
+            //pickUp.set(-0.4);
+            //}
+            
+            if(stick.getRawButton(1)){//
+                launcherSpeed = 0.8;
+                System.out.print("button "+1+" Fired!");    
+            }
+            else if(stick.getRawButton(2)){//
+                launcherSpeed = 0.85;
+                System.out.print("button "+2+" Fired!");
+                }
+            else if(stick.getRawButton(3)){//
+                launcherSpeed = 0.95;
+            System.out.print("button "+3+" Fired!");
         }
-        	else if(stick.getRawButton(4)){//
-        		launcherSpeed = 1;
-        		System.out.print("button "+4+" Fired!");
-        	}
-        	
-        	//BELLOW WILL NOT RUN UNLESS INTIALIZED 
-        	if(stick.getRawButton(8))
-        	{
-        		rightLauncher.set(launcherSpeed-0.1);
-        		leftLauncher.set(launcherSpeed);
-        	}
-        	
-        	
-        	//*******************************
-        	//Launching and buttons to set it
-        	//*******************************
-        	
-        	/*if(stick.getRawButton(8) == true)
-        	{
-            	if(stick.getRawButton(1) == true)
-            	  {
-            	  		launcherSpeed = 0.4;
-            	  }
-            	if(stick.getRawButton(2) == true)
-            	  {
-            	  		launcherSpeed = 0.6;
-            	  }
-            	if(stick.getRawButton(3) == true)
-            	  {
-            	  		launcherSpeed = 1.0;
-            	  }
-        		rightLauncher.set(launcherSpeed);
-        		leftLauncher.set(launcherSpeed);
-        	}*/
-        		
+            else if(stick.getRawButton(4)){//
+                launcherSpeed = 1;
+                System.out.print("button "+4+" Fired!");
+            }
+            
+            //BELLOW WILL NOT RUN UNLESS INTIALIZED 
+            if(stick.getRawButton(8))
+            {
+                rightLauncher.set(launcherSpeed-0.1);
+                leftLauncher.set(launcherSpeed);
+            }
+            
+            
+            //*******************************
+            //Launching and buttons to set it
+            //*******************************
+            
+            /*if(stick.getRawButton(8) == true)
+            {
+                if(stick.getRawButton(1) == true)
+                  {
+                        launcherSpeed = 0.4;
+                  }
+                if(stick.getRawButton(2) == true)
+                  {
+                        launcherSpeed = 0.6;
+                  }
+                if(stick.getRawButton(3) == true)
+                  {
+                        launcherSpeed = 1.0;
+                  }
+                rightLauncher.set(launcherSpeed);
+                leftLauncher.set(launcherSpeed);
+            }*/
+                
             //*********************
-        	//Pick up wheels 
-        	//*********************
-        	
-        	if(stick.getRawButton(7) == true)
-        	{
-            	pickUp.set(.4);
-        	}
+            //Pick up wheels 
+            //*********************
+            
+            if(stick.getRawButton(7) == true)
+            {
+                pickUp.set(.4);
+            }
 
-        	//**********************
-        	//Inverse launcher + Not being pressed
-        	//**********************
-        	
-        	if(stick.getRawButton(6) == true)
-        	{
-        		rightLauncher.set(-1);
-        		leftLauncher.set(-1);
-        	}
-        	if(stick.getRawButton(6)== false && stick.getRawButton(8) == false)
-        	{
-        		rightLauncher.set(0);
-        		leftLauncher.set(0);
-        	}
-        	
-        	
-        	//**********************
-        	//Inverse pickup + When pick up not in use
-        	//**********************
+            //**********************
+            //Inverse launcher + Not being pressed
+            //**********************
+            
+            if(stick.getRawButton(6) == true)
+            {
+                rightLauncher.set(-1);
+                leftLauncher.set(-1);
+            }
+            if(stick.getRawButton(6)== false && stick.getRawButton(8) == false)
+            {
+                rightLauncher.set(0);
+                leftLauncher.set(0);
+            }
+            
+            
+            //**********************
+            //Inverse pickup + When pick up not in use
+            //**********************
 
-        	if(stick.getRawButton(5) == true)
-        	{
-        		pickUp.set(-.4);
-        	}
-        	if(stick.getRawButton(5) == false && stick.getRawButton(7) == false && stick.getRawButton(4) == false)
-			{
-        		pickUp.set(0);
-        	}
+            if(stick.getRawButton(5) == true)
+            {
+                pickUp.set(-.4);
+            }
+            if(stick.getRawButton(5) == false && stick.getRawButton(7) == false && stick.getRawButton(4) == false)
+            {
+                pickUp.set(0);
+            }
 
-            Timer.delay(k_updatePeriod);	// wait 5ms to the next update
+            Timer.delay(k_updatePeriod);    // wait 5ms to the next update
         }
         
     }
